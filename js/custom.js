35882cc2e9fba16ab55fc1f6a80727561daaf4d7
pizza-edition <pizzaeditions@gmail.com>
2025-05-22 14:45:08 +0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,6 @@
 /*
-
 Custom script
-
 This file will not be overwritten by the updater
-
 */
 
 // JavaScript code
@@ -12,7 +9,7 @@ function search_animal() {
   input = input.toLowerCase();
   let x = document.getElementsByClassName("animals");
 
-  for (i = 0; i < x.length; i++) {
+  for (let i = 0; i < x.length; i++) {
     if (!x[i].innerHTML.toLowerCase().includes(input)) {
       x[i].style.display = "none";
     } else {
@@ -20,3 +17,34 @@ function search_animal() {
     }
   }
 }
+
+// PubPower Ads Code
+(function () {
+  var head = document.getElementsByTagName('head')[0];
+
+  // First script
+  var script1 = document.createElement('script');
+  script1.type = 'text/javascript';
+  script1.src = '//nc.pubpowerplatform.io/w/96ccd642-a2ee-4943-a6ca-1da85b4df9da.js';
+  script1.async = true;
+  script1.defer = true;
+  head.appendChild(script1);
+
+  // GDPR config
+  var gdprScript = document.createElement('script');
+  gdprScript.innerHTML = "var powerTag = powerTag || {}; powerTag.gdprShowConsentToolButton = false;";
+  head.appendChild(gdprScript);
+
+  // Second script
+  var script2 = document.createElement('script');
+  script2.type = 'text/javascript';
+  script2.src = '//nc.pubpowerplatform.io/ata/adv/96ccd642-a2ee-4943-a6ca-1da85b4df9da.js';
+  script2.async = true;
+  script2.defer = true;
+  head.appendChild(script2);
+
+  // Sticky banner init
+  var stickyInit = document.createElement('script');
+  stickyInit.innerHTML = "(powerTag.Init = window.powerTag.Init || []).push(function () { powerAPITag.initStickyBanner('pw_47095'); });";
+  head.appendChild(stickyInit);
+})();
